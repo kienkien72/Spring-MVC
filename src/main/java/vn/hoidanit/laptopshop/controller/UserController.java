30360f551c69e28e0b22831d5bcd1dc4352320f5
Kiên <ndkien72@example.com>
2024-09-25 15:15:02 +0700

    Tạo file Static để viết html, viết theo cấu hình MVC

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,22 +1,38 @@
 package vn.hoidanit.laptopshop.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import vn.hoidanit.laptopshop.service.UserSevice;
 
-@RestController
+@Controller
 public class UserController {
+    private UserSevice userSevice;
 
-    private UserSevice userService;
-
-    public UserController(UserSevice userService) {
-        this.userService = userService;
+    public UserController(UserSevice userSevice) {
+        this.userSevice = userSevice;
     }
 
-    @GetMapping("/")
-    public String getHomePage() {
-        return this.userService.helloService();
+    @RequestMapping("/")
+    public String getView() {
+        return "helloMVC.html";
     }
-
 }
+
+// @RestController
+// public class UserController {
+
+// private UserSevice userService;
+
+// public UserController(UserSevice userService) {
+// this.userService = userService;
+// }
+
+// @GetMapping("/")
+// public String getHomePage() {
+// return this.userService.helloService();
+// }
+
+// }
