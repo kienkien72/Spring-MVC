c94c00b74295bab192d4b30d16b5e7967a4d768f
Kiên <ndkien72@example.com>
2024-10-01 14:55:12 +0700

    Tạo Table User, sửa các url

--- src/main/java/vn/ndkien/laptopshop/controller/UserController.java
+++ src/main/java/vn/ndkien/laptopshop/controller/UserController.java
@@ -24,28 +24,28 @@ public class UserController {
 
     @RequestMapping("/")
     public String getHomePage(Model model) {
-        // 2. Lấy tất cả danh sách trong database
-        List<User> arrUser = this.userService.getAllUser(null);
-
         // 3. Lấy tất cả danh sách người dùng có email là ndke@gmail.com
-        List<User> listUserEmail = this.userService.getAllUserEmail("ndke@gmail.com");
-        System.out.println(listUserEmail);
+        // List<User> listUserEmail =this.userService.getAllUserEmail("ndke@gmail.com");
+
         model.addAttribute("kien", "test");
         return "hello";
     }
 
-    @RequestMapping("/admin/user")
+    @RequestMapping("/admin/user/create")
     public String getUserPage(Model model) {
         model.addAttribute("newUser", new User());
         // Lấy từ file create.jsp trong user folder admin
         return "admin/user/create";
     }
 
-    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    @RequestMapping(value = "/admin/user", method = RequestMethod.POST)
     public String createUserPage(Model model, @ModelAttribute("newUser") User ndkien) {
+        // 2. Lấy tất cả danh sách trong database
+        List<User> arrUser = this.userService.getAllUser(null);
+
         System.out.println("kien" + ndkien);
         // 1. Gọi hàm service để lưu vào db
         this.userService.handleUser(ndkien);
-        return "hello";
+        return "admin/user/table-user";
     }
 }
